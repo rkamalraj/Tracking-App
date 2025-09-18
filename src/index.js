a9d627eba9cbb5dd088b717b6ef74cbf9b8ba22e
rkamalraj <rkamalraj12345@gmail.com>
2025-09-18 19:20:02 +0530

    kamal testing 1

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
+//iam kamal fduhfuhfuifu
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
