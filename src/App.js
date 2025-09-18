7c78ac42383bbf719c00e137c214fbeb12e312e9
rkamalraj <rkamalraj12345@gmail.com>
2025-09-18 21:51:40 +0530

    day1 completed

--- src/App.js
+++ src/App.js
@@ -1,42 +1,12 @@
+import React from "react";
+import WeightDashboard from "./components/WeightDashboard";
+
 function App() {
   return (
     <div className="App">
-
-      Hellow World
-
-      <h1>Hello World!</h1>
-    <h1>kamal raj511111</h1>
-
+      <WeightDashboard />
     </div>
   );
 }
 
 export default App;
-
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-
-// <div className="App">
-//     <h1>aimdjfdfjfi </h1>
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
